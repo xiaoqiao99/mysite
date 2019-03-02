1281f27e63d47bcadd59934b06daeefaecb55da1
hui.qiao <hui.qiao@fir.ai>
2019-03-03 00:54:01 +0800

    第一章

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,12 @@
 from django.db import models
+from django.urls import reverse
 from django.utils import timezone
 from django.contrib.auth.models import User
+
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status='published')
 
 
 class Post(models.Model):
@@ -14,10 +20,14 @@ class Post(models.Model):
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
 
+    objects = models.Manager()  # 默认的管理器
+    published = PublishedManager()  # 自定义管理器
 
     class Meta:
         ordering = ('-publish',)
 
-
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.publish.year, self.publish.month, self.publish.day, self.slug])
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 
-# Create your views here.
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+    
+# def post_list(request):
+#     object_list = Post.published.all()
+#     paginator = Paginator(object_list, 3)  # 每页显示3篇文章
+#     page = request.GET.get('page')
+#     try:
+#         posts = paginator.page(page)
+#     except PageNotAnInteger:
+#         # 如果page参数不是一个整数就返回第一页
+#         posts = paginator.page(1)
+#     except EmptyPage:
+#         # 如果页数超出总页数就返回最后一页
+#         posts = paginator.page(paginator.num_pages)
+#     return render(request, 'blog/post/list.html', {'page': page, 'posts': posts})
+
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, slug=post, status="published", publish__year=year, publish__month=month,
+                             publish__day=day)
+    return render(request, 'blog/post/detail.html', {'post': post})
